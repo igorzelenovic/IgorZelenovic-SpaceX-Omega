74bd93324227d814f8cc855ebf31286680a70078
Igor Zelenovic <127208102+igorzelenovic@users.noreply.github.com>
2023-07-06 19:40:27 +0200

    updated

--- src/pages/FlightDetailPage.jsx
+++ src/pages/FlightDetailPage.jsx
@@ -1,20 +1,12 @@
-import React from "react";
+import React from 'react';
 
-const FlightDetailPage = () => {
+function FlightDetailPage() {
   return (
-    <div className="flight-detail-page">
-      <h1 className="page-title">Flight Details</h1>
-      <div className="flight-details">
-        <h2 className="flight-name">Flight Name</h2>
-        <p className="flight-date">Flight Date</p>
-        <p className="flight-description">Flight Description</p>
-      </div>
-      <div className="flight-actions">
-        <button className="button">Action 1</button>
-        <button className="button">Action 2</button>
-      </div>
+    <div>
+      <h1>Flight Details</h1>
+      {/* Prikaz detalja leta */}
     </div>
   );
-};
+}
 
 export default FlightDetailPage;
